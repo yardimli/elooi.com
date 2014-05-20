b309dd3a5b5809b3b1dd738cdef69e1148fd2e9f
Ekim <ekimyardimli@izlenim.com>
2014-05-20 19:48:23 +0800

    cleanup

--- misc/lock-playbar.php
+++
@@ -1,5 +0,0 @@
-<?php require_once("/server-settings.php"); ?>
-<?php 
-if ($_GET["op"]=="lock") { $_SESSION['lockPlaybar']="1"; }
-if ($_GET["op"]=="unlock") { $_SESSION['lockPlaybar']="0"; }
-?>
--- misc/retag-saved.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("/server-settings.php"); ?>
-<?php include("/php-functions.php"); ?>
-<?php require_once('/elooi-translation.php'); ?>
-<?php include("/check-user-login.php"); ?>
-<center>
-<br><br><br><br>
-<?php echo $save_retag_Elooi_Done; ?>
-<br><br><br>
-</center>
